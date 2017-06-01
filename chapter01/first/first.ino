c222e2ed491ef469fed4f55ab777a9ad19b6d398
Johnson Lai <jlwhoo7@gmail.com>
2017-06-02 07:11:29 +0800

    updated first.ino

--- chapter01/first/first.ino
+++ chapter01/first/first.ino
@@ -1,13 +1,13 @@
 // My first programme in Arduino
-const int ledPin = 13;
+const int ledPin = 13; 							// pin 13 is the on board LED.
 
 void setup() {
-	pinMode(ledPin, OUTPUT);
+	pinMode(ledPin, OUTPUT);          // set pin 13 to be OUTPUT
 }
 
-void loop() {
-	digitalWrite(ledPin, LOW);
-	delay(500);
-	digitalWrite(ledPin, HIGH);
-	delay(500);
+void loop() {    										// The loop function Runs over and over forever
+	digitalWrite(ledPin, LOW);        // make ledPin LOW, turn off
+	delay(500);												// wait 0.5 seconds
+	digitalWrite(ledPin, HIGH);       // make ledPin HIGH, turn on
+	delay(500); 											// wait 0.5 seconds
 }
